3cd8ad712624ab736356eaeaeffe801a9b5b59d7
yimadaO_o <yimadaO_o@noreply.gitcode.com>
2024-01-09 22:39:05 -0500

    生成油猴脚本

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,14 @@ export default defineConfig({
     monkey({
       entry: 'src/index.js',
       userscript: {
-        name: 'tracker',
-        version: '2.5.4',
-        description: 'vite-plugin-monkey userscript',
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.baidu.com/']
+        name: '三国杀打小抄',
+        version: '2.5.5',
+        author: '小麦+霜迟',
+        'run-at': 'document-start',
+        description: '打小抄支持网页版和微端，微端文件请加群562224095',
+        icon: 'https://i0.hdslb.com/bfs/new_dyn/17ec41a0ca79633b77399065ab80da3f2138912.png',
+        namespace: 'https://greasyfork.org/scripts/448004',
+        match: ['*://game.iwan4399.com/yxsgs/*', '*://*.sanguosha.com/*']
       },
       server: {
         mountGmApi: false,
