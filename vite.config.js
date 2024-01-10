d5781d06cfa3ab126c09ab4afc65ab0e0890c69a
llsc <48448919+llsccm@users.noreply.github.com>
2024-01-10 18:04:10 +0800

    readme

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       userscript: {
         name: '三国杀打小抄',
         version: '2.5.5',
-        author: '小麦+霜迟',
+        author: '小麦',
         'run-at': 'document-start',
         description: '打小抄支持网页版和微端，微端文件请加群562224095',
         icon: 'https://i0.hdslb.com/bfs/new_dyn/17ec41a0ca79633b77399065ab80da3f2138912.png',
@@ -19,7 +19,6 @@ export default defineConfig({
       },
       server: {
         mountGmApi: false,
-        autoGrant: false
       },
       build: {
         metaFileName: true,
