dbcaf32ff9cfee1497f86e5a0433c229378b2c9e
llsc <48448919+llsccm@users.noreply.github.com>
2024-01-10 21:10:39 +0800

    gameStart

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // import './style.css';
-// import { setupCounter } from './counter.js';
 import { main } from './main.js'
 
-const version = '2.5.4'
 window.WDVerSion = '1.0.0'
 const SGSMODULE = []
 if (!console._log) console._log = console.log
--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,4 @@
 import { addCardTypeButton, clearButton, clearSuit, addQuanBian, addSuit, resetOrderContainer, hideOrderContainer } from './dom'
-import { gameStart } from './gameStart'
 // import skinMap from './map/skinMap'
 import { getCardNumAndSuit, allCardToCurrentMode, currentModeCardType } from './utils/get'
 import { calcResult, JiZhanCal } from './utils/calc'
@@ -7,6 +6,22 @@ import { drawShouPai, drawRemShouPai, drawDingOrDi } from './draw'
 import { removeCardType, addCardType, addCard, removeCard } from './zone'
 import { addFrame, addSkinFrame, updateSkinList, updateSkinListGuoZhan } from './dom'
 
+const deckConfig = {
+  isJunZhengBiaoZhun: { label: '军争', diamond: 41, spade: 40, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isJunZhengBiaoZhunShanShan: { label: '军争', diamond: 41, spade: 40, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isGuoZhanBiaoZhun: { label: '国战', diamond: 27, spade: 27, heart: 27, club: 28, spade2_9: 17, hongsha: 8, heisha: 21 },
+  isGuoZhanYingBian: { label: '国战应变', diamond: 26, spade: 27, heart: 28, club: 28, spade2_9: 17, hongsha: 8, heisha: 21 },
+  isDouDiZhu: { label: '斗地主', diamond: 43, spade: 40, heart: 43, club: 40, spade2_9: 25, hongsha: 18, heisha: 30 },
+  isShenWu: { label: '神武', diamond: 43, spade: 40, heart: 43, club: 40, spade2_9: 25, hongsha: 18, heisha: 30 },
+  isZhuGongSha: { label: '主公杀', diamond: 40, spade: 39, heart: 38, club: 39, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isZhuGongShaShanShan: { label: '主公杀', diamond: 40, spade: 39, heart: 38, club: 39, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isHuanLeBiaoZhun: { label: '军争无木马', diamond: 40, spade: 40, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isHuanLeBiaoZhunShanShan: { label: '军争无木马', diamond: 40, spade: 40, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isJunZhengYingBian: { label: '军争应变', diamond: 41, spade: 40, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isJunZhengYingBianShanShan: { label: '军争应变', diamond: 41, spade: 40, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isShenZhiShiLian: { label: '神之试炼', diamond: 41, spade: 41, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 },
+  isUnknown: { label: '未知牌堆', diamond: 41, spade: 41, heart: 40, club: 40, spade2_9: 25, hongsha: 14, heisha: 30 }
+}
 let mySkin
 let card = {}
 let disableSkinLogic = true
@@ -186,7 +201,7 @@ export function mainLogic(args) {
     currentMode = allCardToCurrentMode(room.cardList)
     currentCardType = currentModeCardType(currentMode)
 
-    gameState = gameStart({ cardList: room.cardList, gameStatusMap })
+    gameState = gameStart(room.cardList)
     for (let i = 1; i <= 3; i++) {
       clearButton('type' + i)
     }
@@ -673,7 +688,7 @@ export function skinLogic(args) {
           if (typeof UsedGeneralSkinID != 'undefined' && typeof UsedGeneralSkinID['UsedGeneralSkinID'][GeneralID] != 'undefined') {
             mySkin = UsedGeneralSkinID['UsedGeneralSkinID'][GeneralID]
             oldGeneralID = GeneralID
-            isFirstTime = true
+            gameState.isFirstTime = true
           } else {
             if (oldGeneralID == 999) {
               mySkin = 0
@@ -681,10 +696,10 @@ export function skinLogic(args) {
               mySkin = UsedGeneralSkinID['UsedGeneralSkinID'][GeneralID]
             }
             oldGeneralID = GeneralID
-            isFirstTime = true
+            gameState.isFirstTime = true
           }
         } else {
-          isFirstTime = false
+          gameState.isFirstTime = false
         }
       }
       console.warn('mySkin: ' + mySkin + ' oldGen: ' + oldGeneralID + ' general: ' + GeneralID + ' isFirstTime: ' + isFirstTime)
@@ -708,9 +723,84 @@ export function skinLogic(args) {
   }
   // TODO 仅仅是控制没用的，还需要恢复点击原来的皮肤，功能还能用
   if (args == '资源组加载完毕：selectSkin') {
-    if (!disableSkinLogic && !isFirstTime && document.getElementById('createSkinIframeSource').contentWindow.document.body.innerHTML != '') {
+    if (!disableSkinLogic && !gameState.isFirstTime && document.getElementById('createSkinIframeSource').contentWindow.document.body.innerHTML != '') {
       document.getElementById('createSkinIframe').style.display = 'inline-block'
       clickToChangeSkinAndCloseSkinFrame()
     }
   }
+}
+
+function gameStart(cardList) {
+  let suits = {
+    diamond: 0,
+    spade: 0,
+    heart: 0,
+    club: 0,
+    spade2_9: 0,
+    hongsha: 0,
+    heisha: 0
+  }
+  for (let i = 0; i < 8; i++) {
+    let seatID = (i + 1).toString()
+    document.getElementById('iframe-source').contentWindow.document.getElementById(seatID).innerHTML = ''
+  }
+  const paidui = new Set()
+  for (const cid of cardList) {
+    paidui.add(cid)
+  }
+  //全部区域清空,牌堆回复张
+  var currentDeckConfig
+  // 找到第一个为 true 的状态
+  for (let key in deckConfig) {
+    if (gameStatusMap[key]) {
+      currentDeckConfig = deckConfig[key]
+      //TODO
+      suits = currentDeckConfig
+      break
+    }
+  }
+  document.getElementById('iframe-source').contentWindow.document.getElementById('nav1').innerHTML = '<b>当前牌堆：' + currentDeckConfig.label + '</b>'
+
+  gameState.qipai = new Set() //zone2 弃牌
+  gameState.chuli = new Set() //zone3 处理区
+  gameState.biaoji = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [] } //key为玩家id，value为zone4 标记区
+  gameState.shoupai = { 0: new Set(), 1: new Set(), 2: new Set(), 3: new Set(), 4: new Set(), 5: new Set(), 6: new Set(), 7: new Set() } //key为seat id而不是id，value为 zone5 手牌区
+  gameState.zhuangbei = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [] } //key为玩家id，value为zone6 装备区
+  gameState.panding = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [] } //key为玩家id，value为zone7 判定区
+  gameState.jineng = new Set() //观星询询会会出现 zone8
+  gameState.zone10 = new Set() //祈禳甘露 zone 10
+  gameState.ding = []
+  gameState.di = []
+  gameState.idOrder = {} //key为玩家id，value为实际座位顺序
+  gameState.seat = 0 //用于座位安排
+  gameState.isGameStart = false
+  //cardType 基本1锦囊2装备3其他4
+  gameState.isSeatOrder = false //座位是否安排好了
+  gameState.isFrameAdd = false
+  gameState.newIdOrder = {}
+  gameState.newShouPai = {}
+  gameState.idOrderPreSet = new Set()
+  gameState.idOrderPre = []
+  gameState.isDuanXian = false
+  gameState.remShouPai = new Set()
+
+  gameState.temShouPai = new Set() //用于处理临时手牌
+  gameState.isDiMeng = false //缔盟
+  gameState.mySeatID = new Set() //用于计算糜竺13点，自己的位置
+  gameState.isFirstTime = true
+  gameState.GuoZhanGeneral = []
+  gameState.boTu = new Set()
+  gameState.enableBoTu = false
+  gameState.luanJi = new Set()
+  gameState.enableLuanJi = false
+  gameState.enableQuanBian = false
+  gameState.quanBian = new Set()
+  gameState.enableHuaMu = false
+  gameState.huaMu = new Set()
+  gameState.unknownCard = []
+  gameState.knownShouPai = new Set()
+  gameState.isClickSkinSelect = false
+  gameState.myID = -1
+  gameState.curGeneral = -1
+  clearSuit()
 }
